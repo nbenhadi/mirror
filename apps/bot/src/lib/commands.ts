7a1860bdf216be28cd621782d5b008114bd839cd
Nourddin Benhadi <nourddinbe@gmail.com>
2026-07-20 19:38:46 +0200

    feat(bot): add /say and /embed admin commands with modal input

--- apps/bot/src/lib/commands.ts
+++ apps/bot/src/lib/commands.ts
@@ -3,10 +3,14 @@ import { password } from '../commands/password.js'
 import { help } from '../commands/help.js'
 import { clear } from '../commands/clear.js'
 import { welcome } from '../commands/welcome.js'
+import { say } from '../commands/say.js'
+import { embed } from '../commands/embed.js'
 
 export const commands = new Map<string, Command>([
   [password.data.name, password],
   [help.data.name, help],
   [clear.data.name, clear],
   [welcome.data.name, welcome],
+  [say.data.name, say],
+  [embed.data.name, embed],
 ])
